d18b4c398474d2b0050c75487ecce3fca43eeeec
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-06-09 19:22:00 +0600

    some changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,9 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2019, text: 'Started my journey', },
+  { year: 2020, text: 'Worked as a freelance developer', },
+  { year: 2021, text: 'Founded Programming hero', },
+  { year: 2022, text: 'Shared Course', },
+  { year: 2022, text: 'Searching For job or intern', },
 ];
