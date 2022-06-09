c67aff4c269baa3bc707a6cf679ff97221decf34
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-06-09 17:25:07 +0600

    name is change

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => (
     <LeftSection>
       <SectionTitle main center>
         Hi <br />
-        I AM Web developer
+        I AM React.js developer
       </SectionTitle>
       <SectionText>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ex quasi quam. Excepturi voluptatibus illo explicabo quo provident minus deserunt.
       </SectionText>
