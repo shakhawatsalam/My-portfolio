af58438be709dca9beb5e85547712a3639937b66
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-06-09 15:23:30 +0600

    tehnologies section is doend

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,46 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked whit a range a technologies in the web development world. 
+      From Back-end To Design
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience With <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem> 
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience With <br />
+            Tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem> 
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience With <br />
+            Node.js and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem> 
+    </List>
+  </Section>
 );
 
 export default Technologies;
